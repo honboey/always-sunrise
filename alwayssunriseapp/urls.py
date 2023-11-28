3dee5afa06c6d1fdaf588db8c1c79a52972f2a35
Hon Boey <hon@dialogicstudios.com.au>
2023-11-28 22:21:48 +1100

    Add index.html

--- alwayssunriseapp/urls.py
+++ alwayssunriseapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  	path('', views.index, name='index'),
   	path('livestream-list', views.livestream_list, name='livestream_list'),
 ]
 
--- alwayssunriseapp/views.py
+++ alwayssunriseapp/views.py
@@ -4,6 +4,11 @@ from .models import Livestream
 
 # Create your views here.
 
+def index(request):
+	livestream = Livestream.objects.get(location__contains="New York")
+	return render(request, 'alwayssunriseapp/index.html', {'livestream': livestream})
+
+
 def livestream_list(request):
 	livestreams = Livestream.objects.filter(local_sunrise_time__lte=timezone.now()).order_by('local_sunrise_time')
 	return render(request, 'alwayssunriseapp/livestream_list.html', {'livestreams': livestreams})
