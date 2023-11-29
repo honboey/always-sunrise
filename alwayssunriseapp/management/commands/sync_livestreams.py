da31786d2197550c8075cc77e4735b992443c0e0
Hon Boey <hon@dialogicstudios.com.au>
2023-11-29 16:48:45 +1100

    WIP Add timezones to datetime

--- alwayssunriseapp/management/commands/sync_livestreams.py
+++ alwayssunriseapp/management/commands/sync_livestreams.py
@@ -1,8 +1,9 @@
 import requests
+import pytz
 
 from django.core.management.base import BaseCommand
 from alwayssunriseapp.models import Livestream
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Command(BaseCommand):
@@ -12,22 +13,65 @@ class Command(BaseCommand):
         livestreams = Livestream.objects.all()
 
         for livestream in livestreams:
+            # Today's sunrise information
+
             sunrise_response_today = requests.get(
                 f"https://api.sunrisesunset.io/json?lat={livestream.latitude}&lng={livestream.longitude}&date=today"
             )
             if sunrise_response_today.status_code == 200:
+                # Convert received time to naive datetime.time object
                 sunrise_time_today = datetime.strptime(
                     sunrise_response_today.json()["results"]["sunrise"], "%I:%M:%S %p"
                 ).time()
-                livestream.local_sunrise_time_today = sunrise_time_today
+
+                # Get timezone and convert to pytz.timezone object
+                local_timezone = pytz.timezone(
+                    sunrise_response_today.json()["results"]["timezone"]
+                )
+
+                # Get today's date and convert to datetime.date
+                today_date = datetime.now().date()
+
+                # Combine time and date to create a naive datetime.datetime object
+                naive_local_sunrise_time_today = datetime.combine(
+                    today_date, sunrise_time_today
+                )
+
+                # Save naive datetime.datetime object to model
+                livestream.local_sunrise_time_today = naive_local_sunrise_time_today
+
+
+                # Add timezone to model
+                livestream.timezone = sunrise_response_today.json()["results"][
+                    "timezone"
+                ]
+
+                print(livestream.location, livestream.local_sunrise_time_today, livestream.local_sunrise_time_today.tzinfo)
+
+            # Tomorrow's sunrise information
 
             sunrise_response_tomorrow = requests.get(
                 f"https://api.sunrisesunset.io/json?lat={livestream.latitude}&lng={livestream.longitude}&date=tomorrow"
             )
             if sunrise_response_tomorrow.status_code == 200:
+                # Convert received time to datetime.time object
                 sunrise_time_tomorrow = datetime.strptime(
-                    sunrise_response_tomorrow.json()["results"]["sunrise"], "%I:%M:%S %p"
+                    sunrise_response_tomorrow.json()["results"]["sunrise"],
+                    "%I:%M:%S %p",
                 ).time()
-                livestream.local_sunrise_time_tomorrow = sunrise_time_tomorrow
+
+                # Get tomorrow's date
+                tomorrow_date = datetime.now().date() + timedelta(days=1)
+
+                # Get timezone
+                local_timezone = pytz.timezone(
+                    sunrise_response_tomorrow.json()["results"]["timezone"]
+                )
+                # print("\n\n\n\n\n\n", local_timezone, type(local_timezone), "\n\n\n")
+
+                # Combine the two to create a datetime.datetime object
+                livestream.local_sunrise_time_tomorrow = datetime.combine(
+                    tomorrow_date, sunrise_time_tomorrow
+                ).replace(tzinfo=local_timezone)
 
             livestream.save()
--- alwayssunriseapp/models.py
+++ alwayssunriseapp/models.py
@@ -1,6 +1,8 @@
+import pytz
+
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
+from datetime import datetime, timedelta
 
 
 class Livestream(models.Model):
@@ -8,10 +10,20 @@ class Livestream(models.Model):
     livestream_url = models.URLField()
     latitude = models.DecimalField(max_digits=11, decimal_places=8, default=-33.867778)
     longitude = models.DecimalField(max_digits=11, decimal_places=8, default=151.21)
-    local_sunrise_time_today = models.TimeField(default="06:56:44")
-    local_sunrise_time_tomorrow = models.TimeField(default="06:56:44")
-    local_time = models.TimeField()
+    timezone = models.CharField(max_length=200)
+    local_sunrise_time_today = models.DateTimeField()
+    utc_sunrise_time_today = models.DateTimeField()
+    local_sunrise_time_tomorrow = models.DateTimeField()
+    utc_sunrise_time_tomorrow = models.DateTimeField()
+    local_time = models.DateTimeField()
     weather = models.CharField(max_length=200)
 
     def __str__(self):
         return self.location
+
+    # def save(self, *args, **kwargs):
+    #     local_timezone = pytz.timezone(self.timezone)
+    #     target_timezone = pytz.UTC
+    #     utc_datetime = self.local_sunrise_time_today.astimezone()
+
+    # print(type(self.local_sunrise_time_today))
