25d24348b575180bd7282ec05720050462b76d79
Hon Boey <hon@dialogicstudios.com.au>
2023-11-30 23:07:52 +1100

    Start to scaffold view function and test to select appropriate livestream

--- alwayssunriseapp/models.py
+++ alwayssunriseapp/models.py
@@ -6,12 +6,11 @@ from datetime import datetime, timedelta
 class Livestream(models.Model):
     location = models.CharField(max_length=200)
     livestream_url = models.URLField()
-    latitude = models.DecimalField(max_digits=11, decimal_places=8, default=-33.867778)
-    longitude = models.DecimalField(max_digits=11, decimal_places=8, default=151.21)
+    latitude = models.DecimalField(max_digits=11, decimal_places=8)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8)
     timezone = models.CharField(max_length=200)
     sunrise_time_today = models.DateTimeField()
     sunrise_time_tomorrow = models.DateTimeField()
-    local_time = models.DateTimeField()
     weather = models.CharField(max_length=200)
 
     def __str__(self):
--- alwayssunriseapp/views.py
+++ alwayssunriseapp/views.py
@@ -5,6 +5,51 @@ from datetime import datetime, timedelta
 
 import pytz
 
+# Common utility functions
+
+
+# Create string of time difference
+def format_time_after_sunrise(timedelta_object):
+    """
+    Given a timedelta object of the difference of time between
+    the current time and the sunrise, render that difference as a string.
+    """
+    # Extract hours and minutes from timedelta
+    hours, minutes = divmod(timedelta_object.seconds // 60, 60)
+
+    # Determine if it's positive or negative
+    is_positive = timedelta_object.total_seconds() >= 0
+
+    # Build the string based on the conditions
+    if hours == 0 and minutes < 60:
+        if is_positive:
+            return f"{minutes}min after sunrise"
+        else:
+            return f"{minutes}min till sunrise"
+    else:
+        time_str = f"{hours}h and {minutes}min"
+        if is_positive:
+            return f"{time_str} after sunrise"
+        else:
+            return f"{time_str} till sunrise"
+
+
+def display_appropriate_livestream(livestream_queryset):
+    """
+    Queryset -> Livestream model
+    Given a queryset of livestream models, choose the one which is closest to its
+    upcoming sunrise time.
+    The conditions are:
+    • Sunrise must be in the future
+    • It's time to sunrise must be the smallest out of all in the queryset
+    """
+    for livestream in livestream_queryset:
+        current_time = datetime.now(pytz.timezone(livestream.timezone))
+        if livestream.sunrise_time_today > current_time:
+            print(f"{livestream} will be deleted")
+            
+
+
 # Create your views here.
 
 
@@ -14,29 +59,7 @@ def index(request):
     time_difference = current_time - livestream.sunrise_time_today
 
     # Create string of time difference
-    def format_time_after_sunrise(timedelta_object):
-        # Extract hours and minutes from timedelta
-        hours, minutes = divmod(timedelta_object.seconds // 60, 60)
-
-        # Determine if it's positive or negative
-        is_positive = timedelta_object.total_seconds() >= 0
-
-        # Build the string based on the conditions
-        if hours == 0 and minutes < 60:
-            if is_positive:
-                return f"{minutes}min after sunrise"
-            else:
-                return f"{minutes}min till sunrise"
-        else:
-            time_str = f"{hours}h and {minutes}min"
-            if is_positive:
-                return f"{time_str} after sunrise"
-            else:
-                return f"{time_str} till sunrise"
-
     time_in_relation_to_sunrise = format_time_after_sunrise(time_difference)
-
-    print(time_in_relation_to_sunrise)
 
     return render(
         request,
@@ -51,8 +74,9 @@ def index(request):
 
 def livestream_list(request):
     livestreams = Livestream.objects.filter(
-        local_sunrise_time__lte=timezone.now()
-    ).order_by("local_sunrise_time")
+        sunrise_time_today__lte=timezone.now()
+    ).order_by("sunrise_time_today")
+
     return render(
         request, "alwayssunriseapp/livestream_list.html", {"livestreams": livestreams}
     )
