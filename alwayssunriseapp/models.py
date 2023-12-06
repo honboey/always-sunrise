4d93d8c53418df86ee837dec25456c2143dee3eb
Hon Boey <hon@dialogicstudios.com.au>
2023-12-06 18:44:06 +1100

    Call Geocode API when saving object #5

--- alwayssunriseapp/models.py
+++ alwayssunriseapp/models.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.db import models
 from datetime import datetime, timedelta
+from .utils.api_retrievals import get_lat_and_long
+
+# Create object
 
 
 class Livestream(models.Model):
     location = models.CharField(max_length=200)
     youtube_id = models.CharField(max_length=20, null=True)
-    latitude = models.DecimalField(max_digits=11, decimal_places=8)
-    longitude = models.DecimalField(max_digits=11, decimal_places=8)
+    latitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
     timezone = models.CharField(max_length=200)
     sunrise_time_today = models.DateTimeField()
     sunrise_time_tomorrow = models.DateTimeField()
@@ -15,3 +18,14 @@ class Livestream(models.Model):
 
     def __str__(self):
         return self.location
+    
+    def save(self, *args, **kwargs):
+        print(get_lat_and_long(self.location))
+        # Call Geocode API to get lat and long of location
+        try:
+            self.latitude, self.longitude = get_lat_and_long(self.location)
+        except:
+            self.latitude = 0.0
+            self.longitude = 0.0
+        
+        super().save(*args, **kwargs)
--- alwayssunriseapp/tests/util_tests.py
+++ alwayssunriseapp/tests/util_tests.py
@@ -1,10 +1,5 @@
 from django.test import TestCase
-from django.utils import timezone
-from ..models import Livestream
 from ..utils.api_retrievals import get_lat_and_long
-import pytz
-from datetime import datetime, timedelta
-from freezegun import freeze_time
 
 
 class UtilTests(TestCase):
