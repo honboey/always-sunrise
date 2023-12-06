e59507a4ee8facda930623ecbb0ba82f94d08215
Hon Boey <hon@dialogicstudios.com.au>
2023-12-06 14:09:51 +1100

    Add tests and error handling to geocoding #5

--- alwayssunriseapp/tests/util_tests.py
+++ alwayssunriseapp/tests/util_tests.py
@@ -8,5 +8,14 @@ from freezegun import freeze_time
 
 
 class UtilTests(TestCase):
-    def test_get_lat_and_long(self):
-        get_lat_and_long("Sydney, Australia")
+    def test_get_lat_and_long__city_and_country(self):
+        result = get_lat_and_long("Sydney, Australia")
+        self.assertEqual(result, (-33.8688197, 151.2092955))
+
+    def test_get_lat_and_long__city_only(self):
+        result = get_lat_and_long("Sydney")
+        self.assertEqual(result, (-33.8688197, 151.2092955))
+
+    def test_get_lat_and_long__invalid_search(self):
+        result = get_lat_and_long("als;dkfj")
+        self.assertEqual(result, None)
--- alwayssunriseapp/utils/api_retrievals.py
+++ alwayssunriseapp/utils/api_retrievals.py
@@ -15,6 +15,10 @@ def get_lat_and_long(location_string):
     gmaps = googlemaps.Client(key=google_maps_api_key)
 
     geocode_result = gmaps.geocode(location_string)
-    latitude = geocode_result[0]["geometry"]["location"]["lat"]
-    longitute = geocode_result[0]["geometry"]["location"]["lng"]
-    print(latitude, longitute)
+    try:
+        latitude = geocode_result[0]["geometry"]["location"]["lat"]
+        longitude = geocode_result[0]["geometry"]["location"]["lng"]
+    except:
+        print("There's an error in getting the lat and long")
+        return None
+    return (latitude, longitude)
